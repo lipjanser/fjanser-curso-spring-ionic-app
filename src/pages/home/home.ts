60b9085eb6d17df1dbfdffaeab988df2566eed51
Felipe Janser <felipejanser@gmail.com>
2020-06-03 12:03:07 -0300

    Navegacao

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController, IonicPage } from 'ionic-angular';
+import { CategoriasPage } from '../categorias/categorias';
 
 @IonicPage()
 @Component({
@@ -8,18 +9,16 @@ import { NavController, IonicPage } from 'ionic-angular';
 })
 export class HomePage {
 
-  pages: Array<{title: string, component: any}>;
-
   constructor(public navCtrl: NavController) {
-    this.pages = [
-      { title: 'Home', component:'HomePage' },
-    ];
+
   }
 
-  openPage(page) {
-    // Reset the content nav to have just this page
-    // we wouldn't want the back button to show in this scenario
-    // this.nav.setRoot(page.component);
-    this.navCtrl.push(page.component);
+  login() {
+    this.navCtrl.setRoot('CategoriasPage');
   }
+
+  register() {
+    
+  }
+
 }
