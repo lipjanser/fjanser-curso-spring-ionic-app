ca1b54f68c01cfc652a9fe4d93c95939cfa5c1c5
Felipe Janser <felipejanser@gmail.com>
2020-06-05 08:50:59 -0300

    Mostrando as categorias

--- src/config/api.config.ts
+++ src/config/api.config.ts
@@ -1,3 +1,8 @@
 export const API_CONFIG = {
-    baseUrl: "http://localhost:8080"
+    baseUrl: "http://localhost:8080",
+    bucketBaseUrl: "https://fjanser-spring-ionic.s3-sa-east-1.amazonaws.com",
+    bucketCategoriasBaseUrl: "https://fjanser-spring-ionic.s3-sa-east-1.amazonaws.com/categorias_img",
+    bucketProdutosBaseUrl: "https://fjanser-spring-ionic.s3-sa-east-1.amazonaws.com/produtos_img",
+    bucketClientesBaseUrl: "https://fjanser-spring-ionic.s3-sa-east-1.amazonaws.com/clientes_img",
+    bucketAvatarBlankUrl: "https://fjanser-spring-ionic.s3-sa-east-1.amazonaws.com/avatar-blank.png"
 }
